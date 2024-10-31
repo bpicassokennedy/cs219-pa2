1e5fa245bc0f7e26c23a1cf77c87bbd4f5d93f61
bpicassokennedy <b3llakennedy@gmail.com>
2024-10-31 01:21:32 +0000

    im sick

--- helpers.cpp
+++ helpers.cpp
@@ -16,38 +16,36 @@ int readData(ifstream& ifile, string operations[], uint32_t operand1[], uint32_t
   return count;
 }
 
-//detecting unsigned overflow by passing through the operands and checking if either of the operands are less than the result. returning true if there is an //overflow (an operand is less than the result), false otherwise
-
-bool detectOverflow(uint32_t num1, uint32_t num2){
-  bool overflow = 0;
-  uint32_t result = num1 + num2;
-  if(result < num1 || result < num2){ // again, result should never be less than one of the operands!
-    overflow = 1;
-  }
-  return overflow;
+void updateFlags(const string& operation, uint32_t result){
+    bool neg = 0;
+    bool zero = 0;
+    if(operation.back() == 'S'){
+        neg = (result & 0x80000000) != 0;
+        zero = (result == 0);
+    }
+    cout << "N: " << neg << " Z: " << zero << endl;
 }
 
 //performing the declared operation on the operands and storing the result in the results array incase these values need to be utilized later on! with that //being said this function allows for the implementation of (potentially) different operations in the future
 
 void runOperations(string operations[], uint32_t operand1[], uint32_t operand2[], uint32_t results[], int count){
-  for(int i = 0; i < count; i++){
-    uint32_t num = 0;
-    bool overflow = 0;
+    for(int i = 0; i < count; i++){
+        uint32_t num = 0;
+        bool updateFlags = 0;
+
+        if(operations[i] == "ADD" || operations[i] == "ADDS"){
+            num = operand1[i] + operand2[i];
+            results[i] = num;
     
-    if(operations[i] == "ADD"){
-      num = operand1[i] + operand2[i];
-      results[i] = num;
-      overflow = detectOverflow(operand1[i], operand2[i]);
-    
-      cout << "ADD" << setw(6) << "0x" << hex << uppercase << operand1[i] << setw(6) << "0x" << hex << uppercase << operand2[i] << ":" << setw(4);
-      cout << "0x" << hex << uppercase << num << " " << endl; //seperating operation and operands from results (just formatting preference)
-      cout << "Overflow: " << (overflow ? "Yes" : "No") << endl << endl;
+            cout << operations[i] << setw(6) << "0x" << hex << uppercase << operand1[i] << setw(6) << "0x" << hex << uppercase << operand2[i] << ":" << setw(4);
+            cout << "0x" << hex << uppercase << num << " " << endl; //seperating operation and operands from results (just formatting preference)
+            updateFlags(operations[i], num);
+        }
+        else if(operations[i] == "SUB" || operations[i] == "SUBS"){
+            //again, demonstrating incase program calls for further implementation
+        }
+        else{
+            cout << "invalid operation!" << endl;
+        }
     }
-    else if(operations[i] == "SUB"){
-      //again, demonstrating incase program calls for further implementation
-    }
-    else{
-      cout << "invalid operation!" << endl;
-    }
-  }
 }
--- helpers.h
+++ helpers.h
@@ -6,11 +6,12 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <iomanip> //hex formatting 
 #include <cstdint> //uint32_t
 
 using namespace std;
 
 int readData(ifstream& ifile, string operations[], uint32_t operand1[], uint32_t operand2[], int max);
-bool detectOverflow(uint32_t num1, uint32_t num2);
+void updateFlags(const string& operation, uint32_t result);
 void runOperations(string operations[], uint32_t operand1[], uint32_t operand2[], uint32_t results[], int count);
